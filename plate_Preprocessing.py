b6714006592347b8dc3b40212613e6efd9228412
tigerlittle1 <b10502002@chu.edu.tw>
2020-02-13 01:15:35 +0800

    前處理修改

--- plate_Preprocessing.py
+++ plate_Preprocessing.py
@@ -33,7 +33,7 @@ class Preprocessing:
         equ = cv2.cvtColor(ycrcb, cv2.COLOR_YCR_CB2BGR)
         return equ
 
-    def rotImg(self,img):
+    def rotImg(self):
         
 #        print('Original Dimensions : ',img.shape)
 #        scale_percent = 1000 # percent of original size
@@ -123,69 +123,72 @@ class Preprocessing:
 #        #cv2.imwrite('sift_keypoints.jpg',img)
 #        return dst
             
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        skew_h, skew_v = skew_detection(gray)
-        corr_img = v_rot(img, (90 - skew_v + skew_h), img.shape, 30);
-        corr_img = h_rot(corr_img, skew_h)
-        return corr_img
-#        img = cv2.imread(r'C:\Users\tiger\Downloads\main6\61-0.jpg', cv2.IMREAD_UNCHANGED)
-#        print('Original Dimensions : ',img.shape)
-#        img = self.hisEqulColor(img)
-#        scale_percent = 1000 # percent of original size
-#        width = int(img.shape[1] * 2)
-#        height = int(img.shape[0] * 2)
-#        dim = (width, height)
-#        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 #        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#        #gray = cv2.GaussianBlur(gray, (3, 3), 0)
-#        
-#        # 畫線:
-#        FilterSize = 3
-#        v = np.median(img)
-#        sigma=0.55
-#        # 选择合适的lower和upper值，然后应用它们
-#        lower = int(max(0, (1.0 - sigma) * v))
-#        upper = int(min(255, (1.0 + sigma) * v))
-#        edges = cv2.Canny(gray, lower, upper, FilterSize)
-#        lines = cv2.HoughLinesP(edges ,rho = 1,theta = 1*np.pi/180,threshold = 80,minLineLength = 80,maxLineGap = 30)
-#        tx1 = ty1 = tx3 = ty3 = img.shape[1]
-#        tx2 = ty2 = tx4 = ty4 = 0
-#        
-#        for i in range(len(lines)):
-#            x1 = lines[i][0][0]
-#            y1 = lines[i][0][1]    
-#            x2 = lines[i][0][2]
-#            y2 = lines[i][0][3]
-#            if ((y1 < img.shape[0]*0.3 or y1 > img.shape[0]*0.7) and (y2 < img.shape[0]*0.3 or y2 > img.shape[0]*0.7)):
-#                if(y1 < img.shape[0]*0.3):
-#                    if(tx1 > x1 or ty1 > y1):
-#                        tx1 = x1
-#                        ty1 = y1
-#                    if(tx2 < x2 or ty2 < y2):
-#                        tx2 = x2
-#                        ty2 = y2   
-#                if(y1 > img.shape[0]*0.7):
-#                    if(tx3 > x1 or ty3 > y1):
-#                        tx3 = x1
-#                        ty3 = y1
-#                    if(tx4 < x2 or ty4 < y2):
-#                        tx4 = x2
-#                        ty4 = y2      
-#                #cv2.line(img,(x1,y1),(x2,y2),(0,0,255),1)
-#        print(tx1 ,ty1 , tx2 , ty2 , tx3 , ty3 , tx4 , ty4)
-#        #轉正
-#        print(img.shape[0], img.shape[1])
-#        pts1 = np.float32([[0 ,ty1],[img.shape[1] , ty2],[0 , ty3], [img.shape[1] , ty4]])
-#        pts2 = np.float32([[0, 0], [img.shape[1], 0], [0, img.shape[0]], [img.shape[1], img.shape[0]]])
-#        M = cv2.getPerspectiveTransform(pts1,pts2)
-#        dst = cv2.warpPerspective(img,M,(img.shape[1], img.shape[0]))
-##        print(type(dst))
-##        cv2.imshow("1",dst)
-##        cv2.waitKey(1)
-#        return dst
+#        skew_h, skew_v = skew_detection(gray)
+#        corr_img = v_rot(img, (90 - skew_v + skew_h), img.shape, 30);
+#        corr_img = h_rot(corr_img, skew_h)
+#        return corr_img
+        img = cv2.imread(r'C:\Users\harry\Desktop\3.jpg', cv2.IMREAD_UNCHANGED)
+        print('Original Dimensions : ',img.shape)
+        #img = self.hisEqulColor(img)
+        scale_percent = 1000 # percent of original size
+        width = int(img.shape[1] * 2)
+        height = int(img.shape[0] * 2)
+        dim = (width, height)
+        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #gray = cv2.GaussianBlur(gray, (3, 3), 0)
+        
+        # 畫線:
+        FilterSize = 3
+        v = np.median(img)
+        sigma=0.55
+        # 选择合适的lower和upper值，然后应用它们
+        lower = int(max(0, (1.0 - sigma) * v))
+        upper = int(min(255, (1.0 + sigma) * v))
+        edges = cv2.Canny(gray, lower, upper, FilterSize)
+        lines = cv2.HoughLinesP(edges ,rho = 1,theta = 1*np.pi/180,threshold = 80,minLineLength = 80,maxLineGap = 30)
+        tx1 = ty1 = tx3 = ty3 = img.shape[1]
+        tx2 = ty2 = tx4 = ty4 = 0
+        
+        for i in range(len(lines)):
+            x1 = lines[i][0][0]
+            y1 = lines[i][0][1]    
+            x2 = lines[i][0][2]
+            y2 = lines[i][0][3]
+            if ((y1 < img.shape[0]*0.3 or y1 > img.shape[0]*0.7) and (y2 < img.shape[0]*0.3 or y2 > img.shape[0]*0.7)):
+                if(y1 < img.shape[0]*0.3):
+                    if(tx1 > x1 or ty1 > y1):
+                        tx1 = x1
+                        ty1 = y1
+                    if(tx2 < x2 or ty2 < y2):
+                        tx2 = x2
+                        ty2 = y2   
+                if(y1 > img.shape[0]*0.7):
+                    if(tx3 > x1 or ty3 > y1):
+                        tx3 = x1
+                        ty3 = y1
+                    if(tx4 < x2 or ty4 < y2):
+                        tx4 = x2
+                        ty4 = y2      
+                cv2.line(img,(x1,y1),(x2,y2),(0,0,255),1)
+        print(tx1 ,ty1 , tx2 , ty2 , tx3 , ty3 , tx4 , ty4)
+        #轉正
+        cv2.imshow("1",img)
+        cv2.waitKey(1)
+        print(img.shape[0], img.shape[1])
+        pts1 = np.float32([[0 ,ty1],[img.shape[1] , ty2],[0 , ty3], [img.shape[1] , ty4]])
+        pts2 = np.float32([[0, 0], [img.shape[1], 0], [0, img.shape[0]], [img.shape[1], img.shape[0]]])
+        M = cv2.getPerspectiveTransform(pts1,pts2)
+        dst = cv2.warpPerspective(img,M,(img.shape[1], img.shape[0]))
+#        print(type(dst))
+        cv2.imshow("2",dst)
+        cv2.waitKey(1)
+        return dst
 
 
-#p = Preprocessing()
+p = Preprocessing()
+p.rotImg()
 #input_path = r"G:\YOLO_plate\pro"#資料夾目錄
 #output_path = r"G:\YOLO_plate\plate\light_pro\train"
 #files= os.listdir(input_path) #得到資料夾下的所有檔名稱
